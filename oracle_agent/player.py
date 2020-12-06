a627df61f6e70e152fa8afded18ec94f42530ded
AndrewNolte <anolte512@gmail.com>
2020-12-06 11:40:54 -0600

    added loc sharing code

--- oracle_agent/player.py
+++ oracle_agent/player.py
@@ -15,17 +15,31 @@ class HockeyPlayer(object):
     BRAKE_ANGLE = 30
     DEFENSE_RADIUS = 40
 
+    PUCK = None
+    PUCK_T = 0
+
     def __init__(self, player_id=0, kart='wilber'):
+        # constants
         self.player_id = player_id
         self.kart = kart
         self.team = player_id % 2
-        self.offense = True #not player_id < 2
+        self.own_goal = np.float32([0, -65 if self.team == 0 else 65])
         self.goal = np.float32([0, 64 if self.team == 0 else -64])
         self.model = models.Detector()
         self.model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), "..", path.join('solution', 'det.th')), map_location='cpu'))
         # self.model = self.model.cuda()
-        self.own_goal = np.float32([0, -65 if self.team == 0 else 65])
+
+        # states
+        self.offense = (player_id == 0)
+        self.pucklock = True
+
+        # vars
+        self.puck = np.float32([0, 0])
+        self.t = 0
+
+        # recurrent info
         self.last_puck = None
+        self.last_loc = None 
 
     def act(self, image, player_info, game_state=None, mask=None):
         """
@@ -37,41 +51,75 @@ class HockeyPlayer(object):
         # dets, depth, is_puck = self.model.detect(torch.from_numpy(image/255.0).float().permute(2, 0, 1).cuda())
         dets, depth, is_puck = self.model.detect(torch.from_numpy(image/255.0).float().permute(2, 0, 1))
 
-        puck = dets[1][0]
-        #print(puck[1]/400)
-        #print(puck[2]/300)
-        puck_updated = False
-        if is_puck > 0 and puck[0] > 2 and len(dets[0]) > 0 and abs(puck[1] - dets[0][0][1]) + abs(puck[2] - dets[0][0][2]) > 45: # and abs(puck[3] - dets[0][0][3]) + abs(puck[4] - dets[0][0][4]) > 10:
-          puck = utils.center_to_world(puck[1], puck[2], 400, 300, np.array(player_info.camera.projection).T @ np.array(player_info.camera.view).T)
-          if puck is not None and np.linalg.norm(puck - player_info.kart.location) > 2:
-            self.last_puck = puck
-            puck_updated = True
-            puck = puck[[0, 2]]
-          else:
-            puck = self.last_puck
-            if puck is not None:
-              puck = puck[[0, 2]]
-            else:
-              puck = np.array([0, 0])
-        else:
-          puck = self.last_puck
-          if puck is None:
-            puck = np.array([0, 0]) # estimate at center before known
-          else:
-            puck = puck[[0, 2]]
-        realpuck = np.float32(game_state.soccer.ball.location)[[0, 2]]
-        puck_map = np.zeros((66 * 4, 66 * 4, 3))
-        puck_map[int(round(-66 + 66 * 2 - 1)):int(round(-66 + 66 * 2 + 133)), int(round(-50 + 66 * 2 - 1)):int(round(-50 + 66 * 2 + 1))] = [100, 0, 0]
-        puck_map[int(round(-66 + 66 * 2 - 1)):int(round(-66 + 66 * 2 + 133)), int(round(-50 + 66 * 2 - 1 + 100)):int(round(-50 + 66 * 2 + 101))] = [100, 0, 0]
-        puck_map[int(round(-66 + 66 * 2 - 1)):int(round(-66 + 66 * 2 + 1)), int(round(-50 + 66 * 2 - 1)):int(round(50 + 66 * 2 + 1))] = [100, 0, 0]
-        puck_map[int(round(66 + 66 * 2 - 1)):int(round(66 + 66 * 2 + 1)), int(round(-50 + 66 * 2 - 1)):int(round(50 + 66 * 2 + 1))] = [100, 0, 0]
-        puck_map[int(round(puck[1] + 66 * 2 - 2)):int(round(puck[1] + 66 * 2 + 2)), int(round(puck[0] + 66 * 2 - 2)):int(round(puck[0] + 66 * 2 + 2))] += [255, 0, 0]
-        puck_map[int(round(realpuck[1] + 66 * 2 - 2)):int(round(realpuck[1] + 66 * 2 + 2)), int(round(realpuck[0] + 66 * 2 - 2)):int(round(realpuck[0] + 66 * 2 + 2))] += [0, 255, 0]
         front = np.float32(player_info.kart.front)[[0, 2]]
         kart = np.float32(player_info.kart.location)[[0, 2]]
-        puck_map[int(round(kart[1] + 66 * 2 - 2)):int(round(kart[1] + 66 * 2 + 2)), int(round(kart[0] + 66 * 2 - 2)):int(round(kart[0] + 66 * 2 + 2))] += [0, 0, 255]
         vel = np.float32(player_info.kart.velocity)[[0, 2]]
         speed = norm(vel)
+
+        if self.last_loc is None or float(norm(kart - self.last_loc)) > 10:
+            self.t = 0
+        self.last_loc = kart
+
+        coords = dets[1][0]
+        proj =  np.array(player_info.camera.projection).T @ np.array(player_info.camera.view).T
+        puck = utils.center_to_world(coords[1], coords[2], 400, 300, proj)
+
+        #print(puck[1]/400)
+        #print(puck[2]/300)
+
+        # TODO: insert some confidence metric here
+        if puck is not None:
+            # print(puck.shape)
+            puck = puck[[0,2]]
+            self.pucklock = True
+
+        if puck is None:
+            otherpuck = HockeyPlayer.PUCK
+            pt = HockeyPlayer.PUCK_T
+            self.pucklock = True
+
+            if abs(self.t - pt) < 10 and otherpuck is not None:
+                puck = otherpuck
+            elif self.t < 60: # change this?
+                # assume puck is at center
+                puck = np.float32([0,0])
+            else:
+                # back up until we see puck
+                self.pucklock = False
+                puck = np.float32([0,0])
+        else:
+            # set shared puck
+            HockeyPlayer.PUCK = puck
+            HockeyPlayer.PUCK_T = self.t
+        # puck_updated = False
+        # if is_puck > 0 and puck[0] > 2 and len(dets[0]) > 0 and abs(puck[1] - dets[0][0][1]) + abs(puck[2] - dets[0][0][2]) > 45: # and abs(puck[3] - dets[0][0][3]) + abs(puck[4] - dets[0][0][4]) > 10:
+        #   puck = utils.center_to_world(puck[1], puck[2], 400, 300, np.array(player_info.camera.projection).T @ np.array(player_info.camera.view).T)
+        #   if puck is not None and np.linalg.norm(puck - player_info.kart.location) > 2:
+        #     self.last_puck = puck
+        #     puck_updated = True
+        #     puck = puck[[0, 2]]
+        #   else:
+        #     puck = self.last_puck
+        #     if puck is not None:
+        #       puck = puck[[0, 2]]
+        #     else:
+        #       puck = np.array([0, 0])
+        # else:
+        #   puck = self.last_puck
+        #   if puck is None:
+        #     puck = np.array([0, 0]) # estimate at center before known
+        #   else:
+        #     puck = puck[[0, 2]]
+        # realpuck = np.float32(game_state.soccer.ball.location)[[0, 2]]
+        puck_map = np.zeros((66 * 4, 66 * 4, 3))
+        # puck_map[int(round(-66 + 66 * 2 - 1)):int(round(-66 + 66 * 2 + 133)), int(round(-50 + 66 * 2 - 1)):int(round(-50 + 66 * 2 + 1))] = [100, 0, 0]
+        # puck_map[int(round(-66 + 66 * 2 - 1)):int(round(-66 + 66 * 2 + 133)), int(round(-50 + 66 * 2 - 1 + 100)):int(round(-50 + 66 * 2 + 101))] = [100, 0, 0]
+        # puck_map[int(round(-66 + 66 * 2 - 1)):int(round(-66 + 66 * 2 + 1)), int(round(-50 + 66 * 2 - 1)):int(round(50 + 66 * 2 + 1))] = [100, 0, 0]
+        # puck_map[int(round(66 + 66 * 2 - 1)):int(round(66 + 66 * 2 + 1)), int(round(-50 + 66 * 2 - 1)):int(round(50 + 66 * 2 + 1))] = [100, 0, 0]
+        # puck_map[int(round(puck[1] + 66 * 2 - 2)):int(round(puck[1] + 66 * 2 + 2)), int(round(puck[0] + 66 * 2 - 2)):int(round(puck[0] + 66 * 2 + 2))] += [255, 0, 0]
+        # puck_map[int(round(realpuck[1] + 66 * 2 - 2)):int(round(realpuck[1] + 66 * 2 + 2)), int(round(realpuck[0] + 66 * 2 - 2)):int(round(realpuck[0] + 66 * 2 + 2))] += [0, 255, 0]
+        # puck_map[int(round(kart[1] + 66 * 2 - 2)):int(round(kart[1] + 66 * 2 + 2)), int(round(kart[0] + 66 * 2 - 2)):int(round(kart[0] + 66 * 2 + 2))] += [0, 0, 255]
+
 
         u = front - kart
         u /= norm(u)
