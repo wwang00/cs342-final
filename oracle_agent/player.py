2896d411e34987549835dd5a1e5338eb8dadcd65
AndrewNolte <anolte512@gmail.com>
2020-12-06 23:20:18 -0600

    30/30 twice

--- oracle_agent/player.py
+++ oracle_agent/player.py
@@ -5,42 +5,49 @@ import oracle_agent.utils as utils
 import sys
 # sys.path.insert(1, './solution')
 # import models
-import oracle_agent.models as models
-
+from oracle_agent.models import Detector
 import torch
 from os import path
+import torch
 
 
 class HockeyPlayer(object):
-    TARGET_SPEED = 15
+    TARGET_SPEED = 25
     DRIFT_ANGLE = 20
-    BRAKE_ANGLE = 30
+    BRAKE_ANGLE = 20
     DEFENSE_RADIUS = 40
+    ZOOM_TIME = 30
 
     PUCK = None
     PUCK_T = 0
 
-    def __init__(self, player_id=0, kart='wilber'):
+    def __init__(self, player_id=0, kart='tux'):
         # constants
         self.player_id = player_id
         self.kart = kart
         self.team = player_id % 2
         self.own_goal = np.float32([0, -65 if self.team == 0 else 65])
         self.goal = np.float32([0, 64 if self.team == 0 else -64])
-        self.model = models.Detector()
+        self.model = Detector()
         self.model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'det.th'), map_location='cpu'))
+        # self.model.load_state_dict(torch.load('det.th'))
+        # self.model = self.model.to(torch.device('cuda'))
         self.model = self.model.cuda()
 
+        self.model.eval()
+
         # states
-        self.offense = (player_id == 0)
+        self.offense = (player_id >= 2)
         self.pucklock = True
 
         # vars
         self.puck = np.float32([0, 0])
         self.t = 0
+        self.delta_puck = np.float32([0, 0])
+        self.last_seen = 0
 
         # recurrent info
-        self.last_puck = None
+        self.last_puck =  np.float32([0, 0])
         self.last_loc = None 
 
     def act(self, image, player_info, game_state=None, mask=None):
@@ -49,9 +56,9 @@ class HockeyPlayer(object):
         :param player_info: pystk.Player object for the current kart.
         return: Dict describing the action
         """
-        # puck = np.float32(HACK_DICT['state'].soccer.ball.location)[[0, 2]]
         dets, depth, is_puck = self.model.detect(torch.from_numpy(image/255.0).float().permute(2, 0, 1).cuda())
         # dets, depth, is_puck = self.model.detect(torch.from_numpy(image/255.0).float().permute(2, 0, 1))
+
 
         front = np.float32(player_info.kart.front)[[0, 2]]
         kart = np.float32(player_info.kart.location)[[0, 2]]
@@ -60,69 +67,42 @@ class HockeyPlayer(object):
 
         if self.last_loc is None or float(norm(kart - self.last_loc)) > 10:
             self.t = 0
+        self.t+=1
+
         self.last_loc = kart
 
         coords = dets[1][0]
         proj =  np.array(player_info.camera.projection).T @ np.array(player_info.camera.view).T
         puck = utils.center_to_world(coords[1], coords[2], 400, 300, proj)
 
-        #print(puck[1]/400)
-        #print(puck[2]/300)
-
-        # TODO: insert some confidence metric here
-        if puck is not None:
-            # print(puck.shape)
+        # if we see puck
+        if puck is not None and dets[1][0][0] > 2 and is_puck > 2:
+            # draw_cords(image, puck, proj)
             puck = puck[[0,2]]
+            self.delta_puck = (puck - self.last_puck) / (self.t - self.last_seen)
+            self.last_puck = puck
+            self.last_seen = self.t
             self.pucklock = True
 
-        if puck is None:
-            otherpuck = HockeyPlayer.PUCK
-            pt = HockeyPlayer.PUCK_T
-            self.pucklock = True
-
-            if abs(self.t - pt) < 10 and otherpuck is not None:
-                puck = otherpuck
-            elif self.t < 60: # change this?
-                # assume puck is at center
-                puck = np.float32([0,0])
-            else:
-                # back up until we see puck
-                self.pucklock = False
-                puck = np.float32([0,0])
+        # if we recently saw puck
+        elif abs(self.last_seen - self.t) < 5:
+            puck = self.last_puck
         else:
-            # set shared puck
-            HockeyPlayer.PUCK = puck
-            HockeyPlayer.PUCK_T = self.t
-        # puck_updated = False
-        # if is_puck > 0 and puck[0] > 2 and len(dets[0]) > 0 and abs(puck[1] - dets[0][0][1]) + abs(puck[2] - dets[0][0][2]) > 45: # and abs(puck[3] - dets[0][0][3]) + abs(puck[4] - dets[0][0][4]) > 10:
-        #   puck = utils.center_to_world(puck[1], puck[2], 400, 300, np.array(player_info.camera.projection).T @ np.array(player_info.camera.view).T)
-        #   if puck is not None and np.linalg.norm(puck - player_info.kart.location) > 2:
-        #     self.last_puck = puck
-        #     puck_updated = True
-        #     puck = puck[[0, 2]]
-        #   else:
-        #     puck = self.last_puck
-        #     if puck is not None:
-        #       puck = puck[[0, 2]]
-        #     else:
-        #       puck = np.array([0, 0])
-        # else:
-        #   puck = self.last_puck
-        #   if puck is None:
-        #     puck = np.array([0, 0]) # estimate at center before known
-        #   else:
-        #     puck = puck[[0, 2]]
-        # realpuck = np.float32(game_state.soccer.ball.location)[[0, 2]]
-        # puck_map = np.zeros((66 * 4, 66 * 4, 3))
-        # puck_map[int(round(-66 + 66 * 2 - 1)):int(round(-66 + 66 * 2 + 133)), int(round(-50 + 66 * 2 - 1)):int(round(-50 + 66 * 2 + 1))] = [100, 0, 0]
-        # puck_map[int(round(-66 + 66 * 2 - 1)):int(round(-66 + 66 * 2 + 133)), int(round(-50 + 66 * 2 - 1 + 100)):int(round(-50 + 66 * 2 + 101))] = [100, 0, 0]
-        # puck_map[int(round(-66 + 66 * 2 - 1)):int(round(-66 + 66 * 2 + 1)), int(round(-50 + 66 * 2 - 1)):int(round(50 + 66 * 2 + 1))] = [100, 0, 0]
-        # puck_map[int(round(66 + 66 * 2 - 1)):int(round(66 + 66 * 2 + 1)), int(round(-50 + 66 * 2 - 1)):int(round(50 + 66 * 2 + 1))] = [100, 0, 0]
-        # puck_map[int(round(puck[1] + 66 * 2 - 2)):int(round(puck[1] + 66 * 2 + 2)), int(round(puck[0] + 66 * 2 - 2)):int(round(puck[0] + 66 * 2 + 2))] += [255, 0, 0]
-        # puck_map[int(round(realpuck[1] + 66 * 2 - 2)):int(round(realpuck[1] + 66 * 2 + 2)), int(round(realpuck[0] + 66 * 2 - 2)):int(round(realpuck[0] + 66 * 2 + 2))] += [0, 255, 0]
-        # puck_map[int(round(kart[1] + 66 * 2 - 2)):int(round(kart[1] + 66 * 2 + 2)), int(round(kart[0] + 66 * 2 - 2)):int(round(kart[0] + 66 * 2 + 2))] += [0, 0, 255]
+            # we lost puck :(
+            puck = np.float32([0, 0])
 
 
+        # draw_arr(image, np.array([puck is not None])) # do we see puck
+        # draw_arr(image, np.array([self.t]), ht=50) # time step
+        # draw_arr(image, np.array([dets[1][0][0], is_puck]), ht=70) # confidence
+
+
+
+        if self.offense and self.t < self.ZOOM_TIME:
+            puck = np.float32([0,0])
+
+            
+            
         u = front - kart
         u /= norm(u)
 
@@ -132,6 +112,8 @@ class HockeyPlayer(object):
             puck_goal /= norm(puck_goal)
             kart_puck_dist = norm(puck - kart)
             aim = puck - puck_goal * kart_puck_dist / 2
+            if not self.pucklock:
+                aim = self.own_goal
         else:  # defense
             own_goal_puck = puck - self.own_goal
             own_goal_puck_dist = norm(own_goal_puck)
@@ -145,19 +127,32 @@ class HockeyPlayer(object):
             else:
                 aim = self.goal
 
+        # draw_cords(image, np.array([aim[0], .3, aim[1]]), proj)
+
         # get steer angles
         v = aim - kart
         v /= norm(v)
         theta = np.degrees(np.arccos(np.dot(u, v)))
-        signed_theta = -np.sign(np.cross(u, v)) * np.sign(np.dot(u, vel)) * theta
-        steer = signed_theta / 8
-
+        signed_theta = -np.sign(np.cross(u, v)) * (.4 + np.sign(np.dot(u, vel)) * theta / 8)
+        steer = signed_theta*1.0
         # brake or accelerate
         drift = False
         if self.offense:
-            brake = self.BRAKE_ANGLE <= theta
-            drift = self.DRIFT_ANGLE <= theta and not brake
-            accel = 1 if speed < self.TARGET_SPEED and not brake else 0
+            if self.t < self.ZOOM_TIME:
+                brake = False
+                drift = False
+                accel = 1
+            elif self.pucklock:
+                brake = self.BRAKE_ANGLE <= theta
+                drift = self.DRIFT_ANGLE <= theta and not brake
+                accel = 1 if speed < self.TARGET_SPEED and not brake else 0
+
+            else:
+                brake = True
+                drift = False
+                accel = 0
+
+                # steer = -1
         else:  # defense
             if own_goal_puck_dist < self.DEFENSE_RADIUS:
                 brake = self.BRAKE_ANGLE <= theta
@@ -177,5 +172,5 @@ class HockeyPlayer(object):
             'drift': False,
             'nitro': False,
             'rescue': False,
-            # 'puck_map': puck_map
+            'fire': True
         }
